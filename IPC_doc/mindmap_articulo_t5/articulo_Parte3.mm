f35f6f549aa178e07e14b9a60c669339a8dfc911
david <davidramirez_28@hotmail.com>
2014-11-06 15:10:04 +0100

    subo mi parte del mindmap

---
+++ IPC_doc/mindmap_articulo_t5/articulo_Parte3.mm
@@ -0,0 +1,196 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1415265904488" ID="ID_1147318577" MODIFIED="1415268428780" STYLE="fork" TEXT="Modelo de proceso de la ingenier&#xed;a de la usabilidad y de la accesibilidad">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1415266359321" ID="ID_1672419984" MODIFIED="1415271033620" POSITION="right" STYLE="fork" TEXT="Accesibilidad">
+<edge COLOR="#ff9900" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1415266538950" ID="ID_1328035171" MODIFIED="1415268066988" TEXT="Motivaci&#xf3;n del dise&#xf1;o accesible">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1415267033318" ID="ID_1112377696" MODIFIED="1415268391592" TEXT="Beneficio social">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1415268297984" ID="ID_1229328998" MODIFIED="1415268381206" TEXT="Personas con discapacidades son m&#xe1;s independientes">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415267046530" ID="ID_152309627" MODIFIED="1415268393856" TEXT="Regulaci&#xf3;n por ley">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1415267866183" ID="ID_329449968" MODIFIED="1415268284876" TEXT="Aplicada a aplicaciones inform&#xe1;ticas que presten servicios p&#xfa;blicos">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415267896242" ID="ID_987566203" MODIFIED="1415268284051" TEXT="Normativas para industria y comercio electr&#xf3;nico en desarrollo">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415268095155" ID="ID_331954556" MODIFIED="1415268122354" TEXT="Normativas en Espa&#xf1;a">
+<node CREATED="1415268124565" ID="ID_1321020880" MODIFIED="1415268418397" TEXT="Norma UNE EX 139802">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415268147499" ID="ID_856687572" MODIFIED="1415268419116" TEXT="Iniciativa e-Europe">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415268168663" ID="ID_590449975" MODIFIED="1415268419766" TEXT="Plan de Acci&#xf3;n Info XXI">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415268189211" ID="ID_1183508981" MODIFIED="1415268420426" TEXT="Declaraci&#xf3;n de Madrid">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415268203747" ID="ID_1504215969" MODIFIED="1415268421066" TEXT="LSSICE - Ley de servicios de la sociedad de la informaci&#xf3;n y de comercio electr&#xf3;nico">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1415267112371" ID="ID_1816721492" MODIFIED="1415268394813" TEXT="Beneficio para todos los usuarios">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1415267815758" ID="ID_911421444" MODIFIED="1415268278784" TEXT="Todo colectivo se ve beneficiado por las adaptaciones que se realicen">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415267142141" ID="ID_715818149" MODIFIED="1415268395783" TEXT="Beneficio tecnol&#xf3;gico">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-4"/>
+<node CREATED="1415267666849" ID="ID_677397260" MODIFIED="1415268269235" TEXT="Independiente del software o hardware">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415267728511" ID="ID_1268653197" MODIFIED="1415268273172" TEXT="Dise&#xf1;o enfocado a gran variedad de dispositivos">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415267163323" ID="ID_825736851" MODIFIED="1415268399091" TEXT="Beneficio econ&#xf3;mico">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-5"/>
+<node CREATED="1415267419102" ID="ID_1149213382" MODIFIED="1415268275399" TEXT="Apertura a nuevos clientes y mercados">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415267207331" ID="ID_380210395" MODIFIED="1415268400027" TEXT="Aspecto de inter&#xe9;s">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-6"/>
+<node CREATED="1415267460992" ID="ID_1811024255" MODIFIED="1415268276120" STYLE="fork" TEXT="Apoyado por entidades gubernamentales">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415267485769" ID="ID_1415720008" MODIFIED="1415268277102" TEXT="Nivel nacional e internacional">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1415266604009" ID="ID_1734868939" MODIFIED="1415268066982" TEXT="Accesibilidad y usabilidad">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1415268476471" ID="ID_466715944" MODIFIED="1415268511410" TEXT="Conceptos a tratar de forma conjunta"/>
+<node CREATED="1415268527651" ID="ID_1837065813" MODIFIED="1415268564342" TEXT="Diferentes casos en las aplicaciones">
+<node CREATED="1415268569652" ID="ID_1364732018" MODIFIED="1415269470690" TEXT="Ni usable ni accesible">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1415269242877" ID="ID_13993491" MODIFIED="1415269455328" TEXT="Caso m&#xe1;s com&#xfa;n">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415268593455" ID="ID_1363147972" MODIFIED="1415269472744" TEXT="Usable y no accesible">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1415269290449" ID="ID_1828032691" MODIFIED="1415269456593" TEXT="Se han aplicado t&#xe9;cnicas de usabilidad en el desarrollo">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415269329096" ID="ID_1990786845" MODIFIED="1415269457174" TEXT="No se han tenido en cuenta a todos los usuarios">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415268612888" ID="ID_288910221" MODIFIED="1415269474720" TEXT="Accesible sin ser usable">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1415269353930" ID="ID_1679712398" MODIFIED="1415269457736" TEXT="Tiene en cuanta a usuarios con discapacidades">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415269436115" ID="ID_766430963" MODIFIED="1415269458266" TEXT="Utilizar sus funciones es dif&#xed;cil">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415268641901" ID="ID_505325822" MODIFIED="1415269476698" TEXT="Usable y accesible">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-4"/>
+<node CREATED="1415269163946" ID="ID_1219332152" MODIFIED="1415269458877" TEXT="Caso ideal">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415269180162" ID="ID_666047808" MODIFIED="1415269459476" TEXT="Idea de dise&#xf1;o universal">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1415266662981" ID="ID_81813216" MODIFIED="1415269674273" TEXT="Accesibilidad en internet">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1415269711209" ID="ID_1009074554" MODIFIED="1415270477613" TEXT="Consorcio W3C">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1415269750819" ID="ID_997123151" MODIFIED="1415269789606" TEXT="Desarrolla protocolos de uso com&#xfa;n para la evoluci&#xf3;n e interoperabilidad de la Web"/>
+<node CREATED="1415269796118" ID="ID_2978926" MODIFIED="1415269805199" TEXT="&#xc1;mbito internacional"/>
+<node CREATED="1415269807512" ID="ID_219465746" MODIFIED="1415270540874" TEXT="Grupo de trabajo WAI (Iniciativa para la Accesibilidad de la Web)">
+<node CREATED="1415269995078" ID="ID_1040608774" MODIFIED="1415270062671" TEXT="Gu&#xed;as de ayuda al dise&#xf1;o de">
+<node CREATED="1415270042336" ID="ID_723776575" MODIFIED="1415270446561" TEXT="La web">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1415270150077" ID="ID_1665183666" MODIFIED="1415270458045" TEXT="Normas WCAG">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415270162986" ID="ID_1658313443" MODIFIED="1415270460583" TEXT="Gu&#xed;as de Accesibilidad para el Contenido Web">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415270063840" ID="ID_713880396" MODIFIED="1415270451593" TEXT="Las herramientas de dise&#xf1;o">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1415270212965" ID="ID_1330752044" MODIFIED="1415270463207" TEXT="Normas ATAG">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415270221352" ID="ID_1683176759" MODIFIED="1415270464407" TEXT="Gu&#xed;as de Accesibilidad para herramientas de dise&#xf1;o de p&#xe1;ginas web">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1415270073556" ID="ID_1230010134" MODIFIED="1415270455469" TEXT="Los agentes de usuario">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1415270347585" ID="ID_1523405589" MODIFIED="1415270465251" TEXT="Normas UAAG">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415270369898" ID="ID_1762631788" MODIFIED="1415270466098" TEXT="Gu&#xed;as de Accesibilidad para navegadores y programas multimedia">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1415266423649" ID="ID_1623011915" MODIFIED="1415268066981" POSITION="left" TEXT="Usabilidad">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415266509350" HGAP="-303" ID="ID_235559704" MODIFIED="1415270933729" POSITION="right" STYLE="fork" TEXT="Modelo de proceso" VSHIFT="164">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1415271555524" ID="ID_1805455961" MODIFIED="1415271595545" TEXT="DCU-Dise&#xf1;o Centrado en el Usuario">
+<node CREATED="1415271600594" ID="ID_393271712" MODIFIED="1415271624714" TEXT="El usuario no debe estar en la mente del dise&#xf1;ador"/>
+<node CREATED="1415271627855" ID="ID_754679400" MODIFIED="1415271933205" TEXT="Debe formar parte del desarrollo"/>
+</node>
+<node CREATED="1415271721948" ID="ID_121346230" MODIFIED="1415271753498" TEXT="Interacci&#xf3;n Persona Computador">
+<node CREATED="1415271755116" ID="ID_1336326846" MODIFIED="1415271778396" TEXT="Hasta ahora &quot;adorno&quot; del dise&#xf1;o"/>
+<node CREATED="1415271780474" ID="ID_101402764" MODIFIED="1415271933205" TEXT="Debe ser aplicada de forma integral en el desarrollo"/>
+</node>
+<node CREATED="1415272029529" ID="ID_520484125" MODIFIED="1415272042953" TEXT="Calidad del software">
+<node CREATED="1415272079143" ID="ID_1801375482" MODIFIED="1415272104228" TEXT="Estandarizaci&#xf3;n ISO/IEC 9126-1">
+<node CREATED="1415272105977" ID="ID_1157035396" MODIFIED="1415272163670" TEXT="Calidad del producto de ingenier&#xed;a de software"/>
+<node CREATED="1415272165285" ID="ID_749233110" MODIFIED="1415272183904" TEXT="Calidad de una aplicaci&#xf3;n en funci&#xf3;n de atributos"/>
+<node CREATED="1415272189415" ID="ID_1885479633" MODIFIED="1415272197669" TEXT="Atributos de calidad">
+<node CREATED="1415272199011" ID="ID_1717156370" MODIFIED="1415272205885" TEXT="Funcionalidad"/>
+<node CREATED="1415272207259" ID="ID_1916076301" MODIFIED="1415272211906" TEXT="Fiabilidad"/>
+<node CREATED="1415272214158" ID="ID_1777721155" MODIFIED="1415272260868" TEXT="Usabilidad">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1415272221369" ID="ID_337962525" MODIFIED="1415272226279" TEXT="Eficiencia"/>
+<node CREATED="1415272227628" ID="ID_1829507455" MODIFIED="1415272233954" TEXT="Mantenibilidad"/>
+<node CREATED="1415272250244" ID="ID_366530577" MODIFIED="1415272255627" TEXT="Portabilidad"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
